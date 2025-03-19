5ea56b0a95192d4e27cbf075d7403ec5c8220050
Denys D <ouctek786@gmail.com>
2025-03-19 12:47:27 +0200

    hw4-validation

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,26 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = () => ContactsCollection.find();
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = 'name',
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+  const contacts = await contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder }).exec();
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
 
 export const getContactById = (contactId) =>
   ContactsCollection.findById(contactId);
@@ -8,12 +28,11 @@ export const getContactById = (contactId) =>
 export const createContact = (contactData) =>
   ContactsCollection.create(contactData);
 
-
 export const updateContactById = (contactId, contactPayload, options = {}) =>
   ContactsCollection.findByIdAndUpdate(contactId, contactPayload, {
     new: true,
     ...options,
   });
 
-  export const deleteContactById = (contactId) =>
-    ContactsCollection.findByIdAndDelete(contactId);
+export const deleteContactById = (contactId) =>
+  ContactsCollection.findByIdAndDelete(contactId);
