4906e316b1f3d28313a0ec5a5d1c925c13aa151a
Denys D <ouctek786@gmail.com>
2025-03-20 11:40:58 +0200

    hw4

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -2,7 +2,7 @@ const parseNumber = (number, defaultValue) => {
   const isString = typeof number === 'string';
   if (!isString) return defaultValue;
   const parsedNumber = parseInt(number);
-  if (Number.isNan(parsedNumber)) {
+  if (Number.isNaN(parsedNumber)) {
     return defaultValue;
   }
   return parsedNumber;
